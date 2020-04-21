826b0e44f6ac26c77f262ccf8fc6d5395f4190cb
wanglg <87623562@qq.com>
2020-04-21 18:33:54 +0800

    4.21

--- src/components/Nowplaying/index.vue
+++ src/components/Nowplaying/index.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="movie_body">
-				<ul>
+	
+    <div class="movie_body" ref="reference">
+		
+				<ul >
+					<li class="pwm">{{pullDownMsg}}</li>
 					<li v-for="item in movelist" :key="item.id">
-						<div class="pic_show"><img :src="item.img"></div>
+						<div class="pic_show" @tap="handleToDetail"><img :src="item.img"></div>
 						<div class="info_list">
 							<h2>{{item.nm}}</h2>
 							<p>观众评 <span class="grade">{{item.sc}}</span></p>
@@ -18,11 +21,13 @@
 			</div>
 </template>
 <script>
+import bsscroll from 'better-scroll';
 export default {
 	name:'nowpaying',
 	data(){
 		return {
-			movelist:[]
+			movelist:[],
+			pullDownMsg:''
 		}
 	},
 	mounted(){
@@ -30,8 +35,46 @@ export default {
 			var msg = res.data.msg;
 			if(msg ==='ok'){
 			this.movelist = res.data.data.movieList;
+			this.$nextTick();
+			var bs = new bsscroll(this.$refs.reference,{
+				tap:true,
+				probeType:1
+			});
+			bs.on('scroll',(pos)=>{
+				//console.log('scroll');
+				if(pos.y>30){
+					console.log('scroll');
+					this.pullDownMsg='正在更新。。'
+				}
+			});
+			bs.on('touchEnd',(pos)=>{
+				//console.log('touchEnd');
+				
+				if(pos.y>30){
+					this.axios.get("/api/movieOnInfoList?cityId=11").then((res=>{
+						var msg = res.data.msg;
+						if(msg ==='ok'){
+							this.pullDownMsg='更新成功';
+							setTimeout(()=>{
+							this.movelist = res.data.data.movieList;
+							console.log('touchEnd');
+							this.pullDownMsg = '';
+							},1000)
+							
+						}
+					}))
+				}
+				
+				
+			})
+			
 			}
 		}));
+	},
+	methods:{
+		handleToDetail(){
+			console.log(111)
+		}
 	}
 }
 </script>
@@ -48,4 +91,5 @@ export default {
 .movie_body .info_list img{ width:50px; position: absolute; right:10px; top: 5px;}
 .movie_body .btn_mall , .movie_body .btn_pre{ width:47px; height:27px; line-height: 28px; text-align: center; background-color: #f03d37; color: #fff; border-radius: 4px; font-size: 12px; cursor: pointer;}
 .movie_body .btn_pre{ background-color: #3c9fe6;}
+.movie_body .pwm{border: none;margin: 0px;padding: 0px;}
 </style>
